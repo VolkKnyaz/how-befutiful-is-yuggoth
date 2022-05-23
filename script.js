149a6ff4b78f28d21ea06e9127180fbcf5b970bf
Artem B <54573430+clericlvl2@users.noreply.github.com>
2022-05-23 17:53:33 +0700

    Update script.js
    added dynamic background

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+let fearClick = Document.querySelector('.clickScarry');
+let back = Document.querySelector('.quote-block');
+
+const fearFunction = () => {
+  buttonScarry.addEventListener('mousedown',() => {
+  back.style.backgroundImage = 'url(assets/img-scary).jpg';
+  });
+  buttonScarry.addEventListener('mouseup',() => {
+  back.style.backgroundImage = 'url(assets/img-intro).jpg';
+  });
+};
+fearFunction();
