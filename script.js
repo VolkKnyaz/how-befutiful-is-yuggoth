7474b13e86be32c050041729c14c3c0304002e60
Artem B <buar.work@gmail.com>
2022-05-24 23:43:59 +0700

    save

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let scarryText = document.querySelector('.clickScarry');
 let quoteBack = document.querySelector('.quote-block');
+//let cardData = document.querySelector('.card-holder:last-child');
+//let cardData = document.getElementById('#new-character-card')
+//let newCardButton = getElementById('#new-character-button');
 
+// background change function
 const scarryBackChange = () => {
   scarryText.addEventListener('mousedown',() => {
     quoteBack.style.backgroundImage = 'url(assets/img-scarry.jpg)';
@@ -14,4 +18,28 @@ const scarryBackChange = () => {
   });
 };
 
-scarryBackChange();
+// new character card function
+/*
+on click: 3 fields
+1: pic url or default text
+2: name or default name
+3: text or default text
+button: submit
+
+on a new card: left right corner edit & delete buttons
+*/
+
+/*function createForm () {
+  appendChild('div');
+};
+
+function newCharacter () {
+  newCardButton.addEventListener('click', () => {
+    cardData.style.display = 'none';
+    cardForm = cardData.appendChild('div');
+    cardForm.style.border = 'solid 5px red';
+  })
+};
+newCharacter ();
+*/
+scarryBackChange ();
