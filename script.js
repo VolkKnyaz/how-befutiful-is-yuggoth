5b24281b73c53a652b00ceaf5e441799bec79bae
Artem B <buar.work@gmail.com>
2022-05-27 20:05:52 +0700

    added functionality to addCard buttons

--- script.js
+++ script.js
@@ -1,45 +1,50 @@
-let scarryText = document.querySelector('.clickScarry');
-let quoteBack = document.querySelector('.quote-block');
-//let cardData = document.querySelector('.card-holder:last-child');
-//let cardData = document.getElementById('#new-character-card')
-//let newCardButton = getElementById('#new-character-button');
+let textButton = document.querySelector('.change-back-button');
+let quoteBox = document.querySelector('.quote-box');
 
 // background change function
-const scarryBackChange = () => {
-  scarryText.addEventListener('mousedown',() => {
-    quoteBack.style.backgroundImage = 'url(assets/img-scarry.jpg)';
-    quoteBack.style.backgroundSize = 'cover';
-    quoteBack.style.backgroundPosition = '50% 25%';
+// добавить плавную анимацию
+// поменять картинку на более жуткую
+const clickBackChange = () => {
+  textButton.addEventListener('mousedown',() => {
+    quoteBox.style.backgroundImage = 'url(assets/img-scarry.jpg)';
+    quoteBox.style.backgroundSize = 'cover';
+    quoteBox.style.backgroundPosition = '50% 25%';
   });
-  scarryText.addEventListener('mouseup',() => {
-    quoteBack.style.backgroundImage = null;
-    quoteBack.style.backgroundSize = null;
-    quoteBack.style.backgroundPosition = null;
+  textButton.addEventListener('mouseup',() => {
+    quoteBox.style.backgroundImage = null;
+    quoteBox.style.backgroundSize = null;
+    quoteBox.style.backgroundPosition = null;
+  });
+};
+clickBackChange ();
+
+// on a new card: left right corner edit & delete buttons & exit
+// добавить крестик в add-card-data
+// добавить плавную анимацию
+let addCardBox = document.getElementById('card-add');
+let clickImage = document.getElementById('card-add-image');
+let cardIcon = document.querySelector('.exit-icon');
+
+const showDataAddCard = () => {
+  clickImage.addEventListener('click',() => {
+    addCardBox.children[0].style.display = 'none';
+    addCardBox.children[1].style.display = 'contents';
+  });
+};
+// понимать, в какой карте находится exit icon
+const showViewAddCard = () => {
+  cardIcon.addEventListener('click',() => {
+    addCardBox.children[1].style.display = 'none';
+    addCardBox.children[0].style.display = 'contents';
   });
 };
 
-// new character card function
-/*
-on click: 3 fields
-1: pic url or default text
-2: name or default name
-3: text or default text
-button: submit
-
-on a new card: left right corner edit & delete buttons
-*/
-
-/*function createForm () {
-  appendChild('div');
+// функция добавления карты
+const addCard = () => {
+  // получить данные (не нажатие) кнопки сабмит
+  // сформировать новый элемент перед кард
+  // Присвоить классы новым и старым элементам
+  // передать данные кнопок
 };
-
-function newCharacter () {
-  newCardButton.addEventListener('click', () => {
-    cardData.style.display = 'none';
-    cardForm = cardData.appendChild('div');
-    cardForm.style.border = 'solid 5px red';
-  })
-};
-newCharacter ();
-*/
-scarryBackChange ();
+showDataAddCard ();
+showViewAddCard ();
